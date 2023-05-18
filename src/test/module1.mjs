23f402df86462129ee0fc08297754bcfea3e3284
Talha Arif <60745973+TalhaElahi@users.noreply.github.com>
2023-05-18 10:41:34 -0700

    props and props types ,pt6

---
+++ src/test/module1.mjs
@@ -0,0 +1,4 @@
+import xyz, {b,c} from './module2.mjs'
+console.log(xyz);
+console.log(b);
+console.log(c);
---
+++ src/test/module2.mjs
@@ -0,0 +1,7 @@
+const a ="talha";
+const b ="zuhaib";
+const c ="haris";
+const d ="arif";
+
+export default a;
+export {b,c}
