dec41c46cc8f22a54892c0a24b2f5598e442c2da
Talha Arif <60745973+TalhaElahi@users.noreply.github.com>
2023-05-30 18:18:48 +0500

    pt 11(challenges) complete

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
-// import TextForm from "./components/TextForm";
+import TextForm from "./components/TextForm";
 let name = "talha";
 function App() {
   return (
     <>
     <Navbar title="textutilsssssssssssss" aboutText="About Us"/>
 <div className="container">
-{/* <TextForm heading="Enter the Text to Analyze"/> */}
+<TextForm heading="Enter the Text to Analyze"/>
 <About/>
 
 </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -26,6 +26,19 @@ export default function TextForm(props) {
         setText(event.target.value);
 
     }
+    const handleExtraSpaces=()=>{
+      console.log("hi i am handle extra spaes");
+      let newText=text.split(/[ ]+/);
+      setText(newText.join(" "));
+    }
+
+    const handleCopy=()=>{
+      console.log("i m copy");
+      var text =document.getElementById("myBox");
+      text.select();
+      text.setSelectionRange(0,9999);
+      navigator.clipboard.writeText(text.value);
+    }
     const [text, setText]=useState('Enter Text Here');
     // setText("new Text");
   return (
@@ -41,6 +54,10 @@ export default function TextForm(props) {
       <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
       <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
       <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
+      <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
+      <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+
+
 
 
     </div>
