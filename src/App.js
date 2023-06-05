816b45e6b8598d8e1dc48ab6310210b54a472bbd
Talha Arif <60745973+TalhaElahi@users.noreply.github.com>
2023-06-05 22:22:51 +0500

    Changing title dynamically & adding favicons (pt 15)

--- src/App.js
+++ src/App.js
@@ -26,11 +26,14 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enabled", "success");
+      document.title='TextUtil - DarkMode';
     }
     if (mode === "dark") {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
+      document.title='TextUtil - LightMode';
+
     }
   };
   return (
