83e5ceafde0eba98b9589ad282b46d1a151cdbbb
Talha Arif <60745973+TalhaElahi@users.noreply.github.com>
2023-06-06 22:17:51 +0500

    17 complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar(props) {
           {/* <Link className="navbar-brand" to="/">
             {props.title}
           </Link> */}
-          <a className="navbar-brand" href="/">
+          <a className="navbar-brand" href="#">
             {props.title}
           </a>
           <button
