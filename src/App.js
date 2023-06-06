1e00017e197a001a2787b569104087cb8b9f32c6
Talha Arif <60745973+TalhaElahi@users.noreply.github.com>
2023-06-06 19:23:08 +0500

    16(React Router Setup + Usage)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 let name = "talha";
 function App() {
@@ -26,34 +27,45 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enabled", "success");
-      document.title='TextUtil - DarkMode';
+      document.title = "TextUtil - DarkMode";
     }
     if (mode === "dark") {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
-      document.title='TextUtil - LightMode';
-
+      document.title = "TextUtil - LightMode";
     }
   };
   return (
     <>
-      <Navbar
-        title="textutilsssssssssssss"
-        mode={mode}
-        toggleMode={toggleMode}
-        aboutText="About Us"
-      />
-      <div className="container">
-        <Alert alert={alert} />
-        <TextForm
-          showAlert={showAlert}
-          heading="Enter the Text to Analyze"
+      <Router>
+        <Navbar
+          title="textutilsssssssssssss"
           mode={mode}
+          toggleMode={toggleMode}
+          aboutText="About Us"
         />
-        <About />
-      </div>
+        <div className="container">
+          <Alert alert={alert} />
+          <Switch>
+         
+            <Route path="/about">
+              <About />
+            </Route>
 
+            <Route path="/">
+              <TextForm
+              showAlert={showAlert}
+              heading="Enter the Text to Analyze"
+              mode={mode}
+            />
+            </Route>
+
+            
+            {/* <About /> */}
+          </Switch>
+        </div>
+      </Router>
       {/* 
 <Navbar/>  */}
     </>
